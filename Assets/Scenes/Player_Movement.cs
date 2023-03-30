daff05f846a6555a3193d3f56fa8623b703e2a4d
Junseong <david991129@naver.com>
2023-03-30 23:35:12 +0900

    Add Player Move

--- Assets/Scenes/Player_Movement.cs
+++ Assets/Scenes/Player_Movement.cs
@@ -5,19 +5,27 @@ using UnityEngine.InputSystem;
 
 public class Player_Movement : MonoBehaviour
 {
+    [SerializeField] float runSpeed = 10f;
     Vector2 moveInput;
+    Rigidbody2D myRigidbody;
     void Start()
     {
-        
+        myRigidbody = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
-        
+        Run();
     }
 
     void OnMove(InputValue value)
     {
         moveInput =value.Get<Vector2>();
+    }
+
+    void Run()
+    {
+        Vector2 playerVelocity = new Vector2 (moveInput.x * runSpeed , myRigidbody.velocity.y);
+        myRigidbody.velocity = playerVelocity;
     }
 }
