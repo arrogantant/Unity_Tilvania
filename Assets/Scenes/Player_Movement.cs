29e8ec024e6c05936227a1fb38b191420a0da6d1
Junseong <david991129@naver.com>
2023-03-30 22:32:29 +0900

    Add input system

---
+++ Assets/Scenes/Player_Movement.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player_Movement : MonoBehaviour
+{
+    Vector2 moveInput;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void OnMove(InputValue value)
+    {
+        moveInput =value.Get<Vector2>();
+    }
+}
