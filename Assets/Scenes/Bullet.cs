e5e9c78530c86f22793db97e789c9b00fcab5a61
Junseong <david991129@naver.com>
2023-04-04 03:51:56 +0900

    Add Bullet

---
+++ Assets/Scenes/Bullet.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    [SerializeField] float bulletSpeed = 1f;
+    Rigidbody2D myRigidbody;
+    Player_Movement player;
+    float xSpeed;
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+        player = FindObjectOfType<Player_Movement>();
+        xSpeed = player.transform.localScale.x * bulletSpeed;
+    }
+
+    
+    void Update()
+    {
+        myRigidbody.velocity = new Vector2(xSpeed,0f);
+    }
+
+    private void OnTriggerEnter(Collider other) 
+    {
+        if(other.tag == "Enemies")
+        {
+            Destroy(other.gameObject);
+        }
+        Destroy(gameObject);    
+    }
+
+    void OnCollisionEnter2D(Collision2D other) 
+    {
+        Destroy(gameObject);
+    }
+}
